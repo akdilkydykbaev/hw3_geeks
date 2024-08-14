df0e5602406adb447c10badb31ca6de43c158a9a
Akdil <akdilkydykbaev5@gmail.com>
2024-08-14 16:19:56 +0600

    HW

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,26 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        double[] numbers = { 1,1, 2,5, -2,3, -3,1, -4,7, -5,1, 6,2, 8,7, 9,3, 10,1, 11,8, 12,9, 13,7, 18,7};
+        System.out.println("Ваш массив" + numbers);
+        boolean negative = false;
+        double sum = 0.0;
+        int count = 0;
+        for (double number: numbers) {
+            if (!negative) {
+                if (number < 0) {
+                    negative = true;
+                }
+            } else {
+                if (number > 0) {
+                    sum += number;
+                    count++;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+                }
+            }
+        }
+        if (count > 0) {
+            double summ = sum/count;
+            System.out.println(sum + "/" + count + "или" + summ);
         }
     }
 }
